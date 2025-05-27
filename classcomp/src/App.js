f1f5e4c4e9b3b2a66da4bc86b3794c548d095499
SharatRokkam <rokkamsharath1@gmail.com>
2025-05-27 16:11:48 +0530

    class comp

--- classcomp/src/App.js
+++ classcomp/src/App.js
@@ -1,24 +1,9 @@
-import React, { useState } from "react";
 import Counter from "./Counter";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
-  const decrement = () => {
-    setCount(count - 1);
-  };
   return (
     <>
-      <div>
-        <h3>{count}</h3>
-        <button onClick={increment}>+</button>
-        <button onClick={decrement}>-</button>
-
-        <Counter name="Sharat" />
-      </div>
+      <Counter />
     </>
   );
 };
--- classcomp/src/Counter.jsx
+++ classcomp/src/Counter.jsx
@@ -1,25 +1,53 @@
+import React from "react";
+
 class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       count: 0,
+      name: "React",
     };
+    console.log("component is constructed");
   }
-  increment = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-  decrement = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
+
+  componentDidMount() {
+    console.log("ComponentDidMount run");
+    this.interval = setInterval(() => {
+      this.setState((prevState) => ({ count: prevState.count + 1 }));
+    }, 2000);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate: Deciding to re-render or not");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("getSnapshotBeforeUpdate: before DOM updates");
+    // Return a snapshot (can be any value; here, let's return previous count)
+    return { previousCount: prevState.count };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("componentDidUpdate: DOM updated");
+    console.log("snapshot from before update", snapshot);
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount : cleaning up before unmount");
+    clearInterval(this.interval);
+  }
 
   render() {
+    console.log("render : rendering the component");
     return (
-      <>
-        <h3>{this.state.count}</h3>
-        <button onClick={this.increment}>Increment</button>
-        <button onClick={this.decrement}>Decrement</button>
-        <h4>{this.props.name}</h4>
-      </>
+      <div>
+        <h1>LifeCycle Methods</h1>
+        <p>Count : {this.state.count}</p>
+        <button onClick={() => this.setState({ name: "ReactJS" })}>
+          Change Name
+        </button>
+      </div>
     );
   }
 }
